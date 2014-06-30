b06d4dd2676c84b35b59b8138c7a747f60d09021
wencanluo <wencanluo.cn@gmail.com>
2014-06-29 21:42:20 -0400

    add data

---
+++ doc/Summarization.mm
@@ -0,0 +1,7 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1403749450650" ID="ID_1099576377" MODIFIED="1403749491322" STYLE="bubble" TEXT="Summarization">
+<node CREATED="1403749471844" ID="ID_1263877265" MODIFIED="1403749488751" POSITION="right" STYLE="bubble" TEXT="clustering"/>
+<node CREATED="1403749495809" ID="ID_1661609339" MODIFIED="1403749498739" POSITION="right" TEXT="topic modeling"/>
+</node>
+</map>
